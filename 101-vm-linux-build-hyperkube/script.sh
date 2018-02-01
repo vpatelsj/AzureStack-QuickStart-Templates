3ab140d8ecbb2e494120669f1c9adc9dfdb2159c
Vaibhav Patel <vapa@microsoft.com>
2018-02-01 14:28:59 -0800

    removing kubectl and minikube parameters

--- 101-vm-linux-build-hyperkube/script.sh
+++ 101-vm-linux-build-hyperkube/script.sh
@@ -10,25 +10,17 @@ sleep 20
 #############
 # Parameters
 #############
-MINIKUBELINK=${1}
-KUBECTLLINK=${2}
-DOCKERLINK=${3}
-REPOLINK=${4}
-REPOBRANCH=${5}
-DOCKERHUBUSERNAME=${6}
-DOCKERHUBPASSWORD=${7}
-DOCKERHUBREPONAME=${8}
-HYPERKUBEVERSION=${9}
+DOCKERLINK=${1}
+REPOLINK=${2}
+REPOBRANCH=${3}
+DOCKERHUBUSERNAME=${4}
+DOCKERHUBPASSWORD=${5}
+DOCKERHUBREPONAME=${6}
+HYPERKUBEVERSION=${7}
 #############
 # Retry Function
 #############
 retrycmd_if_failure() { for i in 1 2 3 4 5; do $@; [ $? -eq 0  ] && break || sleep 5; done ; }
-
-echo "downloading minikube binary"
-retrycmd_if_failure sudo curl -Lo minikube $MINIKUBELINK && sudo chmod +x minikube && sudo mv minikube /usr/local/bin/
-
-echo "downloading kubectl binary"
-retrycmd_if_failure sudo curl -Lo kubectl $KUBECTLLINK && sudo chmod +x kubectl &&  sudo mv kubectl /usr/local/bin/
 
 echo "update the system"
 retrycmd_if_failure sudo apt-get -y update
